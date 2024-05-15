a96f36dcb2e92b8fb049ef941670c39247e2effe
davids52 <108910250+davids52@users.noreply.github.com>
2024-05-15 15:07:16 +0700

    Update BTL.py

--- BTL.py
+++ BTL.py
@@ -1,6 +1,8 @@
 import random
+import time
 from pyamaze import maze
 from pyamaze import agent
+
 def Corresponding_Position(cur_posi,end):
     #TH1
     if cur_posi[0] >= end[0] and cur_posi[1] > end[1]:
@@ -18,6 +20,7 @@ def Corresponding_Position(cur_posi,end):
         return ['E','S','N','W']
     else:
         return ['S','W','E','N']
+    
 def ChangPosition(cur_posi,navi):
     if navi == 'W':
         return (cur_posi[0],cur_posi[1] -1)
@@ -27,6 +30,7 @@ def ChangPosition(cur_posi,navi):
         return (cur_posi[0] + 1,cur_posi[1])
     else:
         return (cur_posi[0] - 1,cur_posi[1])
+    
 def DFS(start,end,maze,visited= None,path= None):
     if visited is None:
         visited = set()
@@ -48,24 +52,21 @@ def DFS(start,end,maze,visited= None,path= None):
                     return new_path
     visited.remove(start)
     return None
-def print_path(maze,path):
-    for position in path:
-        maze.maze_map[position] = 'p'
-    maze.run()
-x,y = map(int,input("Nhap diem xuat phat x,y:").split())
-i,j = map(int,input("Nhap diem ket thuc i,j:").split())
-m = maze(50,50)
+
+x,y = map(int,input("Type start point x,y:").split())
+i,j = map(int,input("Type end point i,j:").split())
+
+#size cua ma tran owr day
+m = maze(60,60)
 m.CreateMaze(x=i,y=j,loopPercent=100)
+
+start = time.perf_counter()
 path = DFS((x,y),(i,j),m)
-# if path is not None:
-#     print(path[0],end='->')
-#     for i in range(1,len(path)):
-#         print(path[i],end='->')
+end = time.perf_counter()
+print("Execution_time = ",end-start)
 if path is not None:
     a = agent(m,x=x,y=y,footprints=True,shape='arrow',color='yellow')
     m.tracePath({a:path})
-    # b = agent(m,x=i,y=j,footprints=True,filled=True)
-    # m.tracePath({b:path.reverse()})
 else:
     print("No route")
 print(len(path))
